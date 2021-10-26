eb510a9f721aa7c0cedb717db6b92aacb7bdaacf
mutemip <paulmutemi2030@gmail.com>
2021-10-27 01:17:03 +0300

    created profile and ProfileStatuses

---
+++ profiles/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(blank=True, max_length=255)
+    city = models.CharField(blank=True, max_length=55)
+    avatar = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class ProfileStatus(models.Model):
+    userprofile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    status_content = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "statuses"
+
+    def __str__(self):
+        return str(self.userprofile)
