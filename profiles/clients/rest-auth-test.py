716e80f546b290512a1050c4932456a6806f0d59
mutemip <paulmutemi2030@gmail.com>
2021-11-01 23:15:52 +0300

    credentials

--- profiles/clients/rest-auth-test.py
+++ profiles/clients/rest-auth-test.py
@@ -3,10 +3,10 @@ import requests
 
 def client():
     credentials = {
-        "username": "mutemip",
-        "password": "123"
+        "username": "paul",
+        "password": "0706858432@PMM"
     }
-    response = requests.post("http://127.0.0.1:8000/api/rest-auth/login", data=credentials)
+    response = requests.post("http://127.0.0.1:8000/api/rest-auth/login/", data=credentials)
     print("Status Code: ", response.status_code)
     response_data = response.json()
     print(response_data)
