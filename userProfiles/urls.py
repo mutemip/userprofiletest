d6cc211411794c88c8c140a195d7354633277c1a
mutemip <paulmutemi2030@gmail.com>
2021-11-01 22:58:32 +0300

    url conf

--- userProfiles/urls.py
+++ userProfiles/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/rest-auth/', include('rest_auth.urls'))
 ]
 
 from django.conf.urls.static import static
