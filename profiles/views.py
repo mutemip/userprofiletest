7fc864148c3faaa63deeaf9798f1a46e14c1ac47
mutemip <paulmutemi2030@gmail.com>
2021-11-01 23:44:16 +0300

    profile list view

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Profile
+from serializers import ProfileSerialiser
+
+class ProfileListAPIView(generics.ListAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerialiser
+    permission_classes = [IsAuthenticated]
