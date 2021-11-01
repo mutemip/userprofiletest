9855c6fea8e0c00012f0147cd59b93e0f9b09b75
mutemip <paulmutemi2030@gmail.com>
2021-11-01 23:46:56 +0300

    profile app urls conf

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import ProfileListAPIView
 
+
 urlpatterns = [
     path('profile/', ProfileListAPIView.as_view()),
 ]
--- userProfiles/urls.py
+++ userProfiles/urls.py
@@ -4,7 +4,8 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/rest-auth/', include('rest_auth.urls'))
+    path('api/rest-auth/', include('rest_auth.urls')),
+    path('api/', include('profiles.urls'))
 ]
 
 from django.conf.urls.static import static
